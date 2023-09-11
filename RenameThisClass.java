85bbaadd8a0e0d0906ca6e402431e57cc4fad314
Elliot Omark <elliot.omark@mrjaffesclass.com>
2023-09-11 11:34:52 -0700

    gayyyyy

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,11 +8,9 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        String[] array={"Alpha","Bravo","Charlie","Delta","Echo"};
+        String[] array={"Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India"};
         for(int i=0;i<array.length;i++){
             System.out.println(array[i]);
-        
-            
         }
         System.out.println(" ");
         for(int i=0;i<array.length;i++){
@@ -21,7 +19,7 @@ public class RenameThisClass
         System.out.println(" ");
         for(int i=0;i<array.length;i++){
             System.out.print(array[i]);
-            if(i<4) {
+            if(i<array.length-1) {
                 System.out.print("-");
             }
         }
